285f2676185c7866615719024ced1788f5eeda2d
Joseph Chan <cyqjoseph@gmail.com>
2022-12-21 15:04:45 +0800

    abug fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,7 @@
 #config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
     allow do
-      origins 'http://localhost:3000',
-      origins 'https://chimerical-pudding-4ffa6b.netlify.app',
+      origins 'http://localhost:3000','https://chimerical-pudding-4ffa6b.netlify.app'
     
       resource '*',
         headers: :any,
